21d4abaa070a278390cc88208a8ddbb8f9bf231b
ouedraogomaria <116485128+ouedraogomaria@users.noreply.github.com>
2023-03-10 10:55:51 +0100

    add app and server

---
+++ app.js
@@ -0,0 +1,8 @@
+/*const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => res.send('hello nodemon!'));
+app.listen(port, () => console.log(`Lapplication est demarrée sur: https://localhost:${port}` ))*/
