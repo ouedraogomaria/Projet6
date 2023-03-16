8446e6c96fad1a4c1516ecb13da8aff6a09da54f
ouedraogomaria <116485128+ouedraogomaria@users.noreply.github.com>
2023-03-16 09:24:45 +0100

    add Thing and user

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-/*const express = require('express');
+const express = require('express');
 
 const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose');
 
-const port = 3000;
+mongoose.connect('mongodb+srv://Mariatou:<AMxVYxez0zCyA0Em>@atlascluster.mt5niqf.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.get('/', (req, res) => res.send('hello nodemon!'));
-app.listen(port, () => console.log(`Lapplication est demarrée sur: https://localhost:${port}` ))*/
+const Thing = require('./models/thing');  
+
+app.use(express.json());
+app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+app.post('/api/auth/signup', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+app.get('/api/auth/signup', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+module.exports = app;
