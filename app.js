4ff1cb1997530926f43f98544b90d097ff1717ed
ouedraogomaria <116485128+ouedraogomaria@users.noreply.github.com>
2023-03-17 18:20:08 +0100

    add routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-Parser')
-const sauce = require('./models/sauce');
+
 const mongoose = require('mongoose');
+const sauceRoutes = require('./routes/sauce');
 
 mongoose.connect('mongodb+srv://Mariatou:AMxVYxez0zCyA0Em@atlascluster.mt5niqf.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -18,27 +19,6 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-
-app.post('/api/sauces', (req, res, next) => {
-  delete req.body._id;
-  const sauce = new sauce({
-    ...req.body
-  })
-  sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.use('/api/sauces', (req, res, next) => {
-  sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauces/:id', (req, res, next) => {
-  sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
+app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,34 +1,40 @@
-/*const express = require('express');
-
+const express = require('express');
 const router = express.Router();
 const sauce = require('../models/sauce');
 
-  
-  app.post('/api/sauces', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new sauce({
-      ...req.body
-    })
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  
-  app.put('/api/sauces/:id', (req, res, next) => {
-    sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  
-  app.get('/api/sauces/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-  app.get('/api/auth/signup', (req, res, next) => {
-    sauce.find()
-      .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({ error }));
-  });
+router.post('/', (req, res, next) => {
+  delete req.body._id;
+  const sauce = new sauce({
+    ...req.body
+  })
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 
-module.exports = router;*/
+router.put('/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.delete('/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.get('/', (req, res, next) => {
+  sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.get('/:id', (req, res, next) => {
+  sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+});
+
+
+module.exports = router;
